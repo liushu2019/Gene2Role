8344791485bb15ce0c19396b0e3fec3fa1d561d3
xzengComBio <152377919+xzengComBio@users.noreply.github.com>
2023-12-25 09:13:52 +0900

    Update spearman.py
    remove unnecessary comments and debugs

--- python/spearman.py
+++ python/spearman.py
@@ -20,11 +20,12 @@ parser.add_argument("out_file_name",
 parser.add_argument("-correlation_threshold", 
                     type=float, 
                     help="Threshold for filtering out top value*100\% correlations. (default:0.4)",
-                    default=0.4)
+                    default=0.01)
 
 args = parser.parse_args()
 
 df = pd.read_csv(args.file_path, index_col=0).transpose()
+
 # Calculate spearman correlation.
 spearman_corr_matrix = df.corr(method='spearman')
 
@@ -33,36 +34,19 @@ correlations = spearman_corr_matrix.values.flatten()
 correlations = correlations[~np.isnan(correlations)]
 correlations = correlations[correlations != 1]
 
-# Calculate histogram of correlations
-#hist, bin_edges = np.histogram(correlations, bins='auto', density=True)
-
-# Determine top 5% thresholds for positive and negative correlations
-
+# Determine top pairs for positive and negative correlations
 positive_threshold = np.percentile(correlations, 100 - int(args.correlation_threshold*100))
 negative_threshold = np.percentile(correlations, int(args.correlation_threshold*100))
-# print(positive_threshold)
-# print(negative_threshold)
-num_positive_edges_top_1_percent = np.sum(correlations >= positive_threshold)
-num_negative_edges_top_1_percent = np.sum(correlations <= negative_threshold)
 
-# # Plotting
-# plt.hist(correlations, bins='auto', density=True, alpha=0.7, color='blue')
-# plt.axvline(x=positive_threshold, color='red', linestyle='dashed', linewidth=2)
-# plt.axvline(x=negative_threshold, color='green', linestyle='dashed', linewidth=2)
-# plt.title('Frequency Distribution of Correlations in ' + args.out_file_name)
-# plt.xlabel('Spearman Correlation Coefficient')
-# plt.ylabel('Frequency')
-
-# plot_file_name = os.path.join(os.path.dirname(args.file_path), args.out_file_name + "_correlation_histogram.png")
-# plt.savefig(plot_file_name)
-
-print(f"Number of edges in the top {int(args.correlation_threshold*100)}% of positive correlations: {num_positive_edges_top_1_percent}")
-print(f"Number of edges in the top {int(args.correlation_threshold*100)}% of negative correlations: {num_negative_edges_top_1_percent}")
+# print the number of positive and negative edges
+num_positive_edges_top_percent = np.sum(correlations >= positive_threshold)
+num_negative_edges_top_percent = np.sum(correlations <= negative_threshold)
+print(f"Number of edges in the top {int(args.correlation_threshold*100)}% of positive correlations: {num_positive_edges_top_percent}")
+print(f"Number of edges in the top {int(args.correlation_threshold*100)}% of negative correlations: {num_negative_edges_top_percent}")
 
 # Identify high positive and negative correlations
 high_positive_corr = spearman_corr_matrix >= positive_threshold
 high_negative_corr = spearman_corr_matrix <= negative_threshold
-
 high_corr_indices = np.where(high_positive_corr | high_negative_corr)
 
 #Prepare the gene pairs and their correlation signs
